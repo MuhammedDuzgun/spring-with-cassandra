a73b47850f35f9b5bda3dfb1537fb5741afc848d
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2025-05-09 02:34:18 +0300

    Initial commit

---
+++ src/main/java/com/demo/spring_with_cassandra/SpringWithCassandraApplication.java
@@ -0,0 +1,13 @@
+package com.demo.spring_with_cassandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithCassandraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithCassandraApplication.class, args);
+	}
+
+}
